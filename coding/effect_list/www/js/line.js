229fe37b7618efa88c1d7d193339a9300a842e5a
kk-watanabe <kk-watanabe@html-coding.co.jp>
2015-11-04 18:13:22 +0900

    【effect_list】とりあえず３D回した

---
+++ coding/effect_list/www/js/line.js
@@ -0,0 +1,46 @@
+//シーンの作成
+var line = function () {
+	var scene = new THREE.Scene(),
+		mainVisual = document.getElementById('mainVisual'),
+		mainVisual_w = mainVisual.clientWidth,
+		mainVisual_h = mainVisual.clientHeight,
+		lineFrame = document.getElementById('line'),
+		fov = 60,
+		aspect = mainVisual_w / mainVisual_h,
+		near = 1,
+		far = 1000,
+		camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
+
+	//カメラの設定
+	camera.position.set( 0, 0, 50 );
+
+	//レンダリング
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize( mainVisual_w, mainVisual_h );
+	lineFrame.appendChild( renderer.domElement );
+
+	//高原
+	var directionalLight = new THREE.DirectionalLight( 0xffffff );
+	directionalLight.position.set( 0, 0.7, 0.7 );
+	scene.add( directionalLight );
+
+	//物体の追加
+	var geometry = new THREE.CubeGeometry( 30, 30, 30 );
+	var material = new THREE.MeshPhongMaterial( { color: 0xffffff } );
+	var mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
+
+	//表示する
+	renderer.render( scene, camera );
+
+	(function renderLoop () {
+		requestAnimationFrame( renderLoop );
+		mesh.rotation.set(
+			0,
+			mesh.rotation.y + 0.01,
+			mesh.rotation.z + 0.01
+		)
+		renderer.render( scene, camera );
+	} )();
+};
+window.addEventListener( 'DOMContentLoaded', line, false );
