190f5041dd499cd6e66701088f19130c2362c63a
kk-watanabe <kk-watanabe@html-coding.co.jp>
2015-11-05 21:59:04 +0900

    なんか回してみる

--- coding/effect_list/www/js/line.js
+++ coding/effect_list/www/js/line.js
@@ -15,8 +15,9 @@ var line = function () {
 	camera.position.set( 0, 0, 50 );
 
 	//レンダリング
-	var renderer = new THREE.WebGLRenderer();
+	var renderer = new THREE.WebGLRenderer({ alpha: true });
 	renderer.setSize( mainVisual_w, mainVisual_h );
+	renderer.setClearColor( 0x000000, 0 );
 	lineFrame.appendChild( renderer.domElement );
 
 	//高原
@@ -25,22 +26,29 @@ var line = function () {
 	scene.add( directionalLight );
 
 	//物体の追加
-	var geometry = new THREE.CubeGeometry( 30, 30, 30 );
-	var material = new THREE.MeshPhongMaterial( { color: 0xffffff } );
+	var geometry = new THREE.CylinderGeometry( 50, 50, 10, 50, 0, false )
+	var material = new THREE.MeshPhongMaterial( {
+		color: 0xffffff,
+		"wireframe" : true,
+	} );
 	var mesh = new THREE.Mesh( geometry, material );
 	scene.add( mesh );
 
 	//表示する
 	renderer.render( scene, camera );
 
+	var degree = 0;
 	(function renderLoop () {
 		requestAnimationFrame( renderLoop );
+
 		mesh.rotation.set(
 			0,
-			mesh.rotation.y + 0.01,
-			mesh.rotation.z + 0.01
-		)
+			mesh.rotation.y + 0.001,
+			mesh.rotation.z + 0.001
+		);
+
 		renderer.render( scene, camera );
 	} )();
 };
+
 window.addEventListener( 'DOMContentLoaded', line, false );
