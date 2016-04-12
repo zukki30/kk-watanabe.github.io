7ab240ea5895577f311d1e8b546145042257d212
kk-watanabe <kk-watanabe@html-coding.co.jp>
2016-04-12 20:03:19 +0900

    コード表示まで完成

--- coding/style_guide/gulpfile.js
+++ coding/style_guide/gulpfile.js
@@ -1,43 +1,251 @@
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
-var rimraf = require('rimraf');
 var runSequence = require('run-sequence');
+var del = require('del');
 
-// 開発ディレクトリ
-var dev_dir = 'www/';
+var setting = {
+  autoprefixer: {
+      browser: ['last 2 version', 'Explorer >= 8', 'Android >= 4', 'Android 2.3']
+  },
+  browserSync: {
+    // 使わない方はコメントアウトする
+    // proxy: 'environment.yk',
+    server:{
+        baseDir: 'www',
+    },
+  },
+  imagemin: {
+    disabled: true,  // falseでimageminを実行
+    level: 7  // 圧縮率
+  },
+  // css、jsのミニファイの有効化/無効化
+  minify: {
+    css: false,
+    js: false
+  },
+  cssbeautify: {
+    disabled: true,
+    options: {
+      indent: ''
+    }
+  },
+  csscomb: {
+    disabled: true,
+  },
+  path: {
+    base: {
+      src: 'src',
+      dest: 'www'
+    },
+    sass: {
+      src: 'src/sass/**/*.scss',
+      dest: 'www/css/',
+    },
+    js: {
+      src: 'src/js/**/*.js',
+      dest: 'www/js/',
+    },
+    image: {
+      src: 'src/img/**/*',
+      dest: 'www/img/',
+    },
+    lib: {
+      src: 'src/lib/**/*',
+      dest: 'www/lib/',
+    },
+    include: {
+      src: ['src/include/**/*'],
+      dest: 'www/include/',
+    },
+    etc: {
+      src: 'src/etc/**/*',
+      dest: 'www/etc/',
+    },
+    html: {
+      src: ['src/**/*', '!src/assets/**/*']
+    },
+  }
+};
 
+// 画像の圧縮
+gulp.task('imagemin', function(){
+  if(!setting.imagemin.disabled){
+    var imageminOptions = {
+      optimizationLevel: setting.imagemin.lebel
+    };
+
+    return gulp.src(setting.path.image.src)
+      .pipe($.plumber({
+        errorHandler: $.notify.onError("Error: <%= error.message %>") //<-
+      }))
+      .pipe($.changed(setting.path.image.dest))
+      .pipe($.imagemin(imageminOptions))
+      .pipe(gulp.dest(setting.path.image.dest))
+      .pipe(browserSync.reload({stream: true}));
+  }else{
+    return gulp.src(
+        setting.path.image.src
+      )
+      .pipe($.plumber({
+        errorHandler: $.notify.onError("Error: <%= error.message %>") //<-
+      }))
+      .pipe($.changed(setting.path.image.dest))
+      .pipe(gulp.dest(setting.path.image.dest))
+      .pipe(browserSync.reload({stream: true}));
+  }
+});
+
+// SASS
 gulp.task('scss',function(){
-  return gulp.src(dev_dir+'sass/**/*.scss')
+  return gulp.src(setting.path.sass.src)
     .pipe($.plumber({
       errorHandler: $.notify.onError("Error: <%= error.message %>") //<-
     }))
-    .pipe($.sass())
-    .pipe(gulp.dest(dev_dir+'css/'))
+    .pipe($.sass({outputStyle: 'expanded'}))
+    .pipe($.autoprefixer(setting.autoprefixer.browser))
+    .pipe(gulp.dest(setting.path.sass.dest))
     .pipe(browserSync.reload({stream: true}));
 });
 
+// HTML
 gulp.task('html', function(){
-  return gulp.src(dev_dir+'*.html')
+  return gulp.src(
+      setting.path.html.src,
+      {base: setting.path.base.src}
+    )
     .pipe($.plumber({
       errorHandler: $.notify.onError("Error: <%= error.message %>") //<-
     }))
-  .pipe($.changed())
-  .pipe(gulp.dest(setting.path.base.dest))
-  .pipe(browserSync.reload({stream: true}));
+    .pipe($.changed(setting.path.base.dest))
+    .pipe(gulp.dest(setting.path.base.dest))
+    .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('watch',function(){
-  browserSync.init({
-    server:{
-      baseDir: ['www'],
-    }
-  });
+// JavaScript
+gulp.task('js', function(){
+  return gulp.src(
+      setting.path.js.src
+    )
+    .pipe($.plumber({
+      errorHandler: $.notify.onError("Error: <%= error.message %>") //<-
+    }))
+    .pipe($.changed(setting.path.js.dest))
+    .pipe(gulp.dest(setting.path.js.dest))
+    .pipe(browserSync.reload({stream: true}));
+});
 
-  gulp.watch([dev_dir+'sass/**/*.scss'], {interval: 500}, ['scss']);
-  gulp.watch([dev_dir+'**/*.js'], {interval: 500}, browserSync.reload);
-  gulp.watch([dev_dir+'**/*.html'], {interval: 500}, browserSync.reload);
-  gulp.watch([dev_dir+'**/*.php'], {interval: 500}, browserSync.reload);
+// Lib
+gulp.task('lib', function(){
+  return gulp.src(
+      setting.path.lib.src
+    )
+    .pipe($.plumber({
+      errorHandler: $.notify.onError("Error: <%= error.message %>") //<-
+    }))
+    .pipe($.changed(setting.path.lib.dest))
+    .pipe(gulp.dest(setting.path.lib.dest))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+// Include
+gulp.task('include', function(){
+  return gulp.src(
+      setting.path.include.src
+    )
+    .pipe($.plumber({
+      errorHandler: $.notify.onError("Error: <%= error.message %>") //<-
+    }))
+    .pipe($.changed(setting.path.include.dest))
+    .pipe(gulp.dest(setting.path.include.dest))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+// Etc
+gulp.task('etc', function(){
+  return gulp.src(
+      setting.path.etc.src
+    )
+    .pipe($.plumber({
+      errorHandler: $.notify.onError("Error: <%= error.message %>") //<-
+    }))
+    .pipe($.changed(setting.path.etc.dest))
+    .pipe(gulp.dest(setting.path.etc.dest))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+// JS Minify
+gulp.task('jsminify', function(){
+  if(setting.minify.js){
+    return gulp.src(setting.path.js.dest+'**/*.js')
+      .pipe($.plumber({
+        errorHandler: $.notify.onError("Error: <%= error.message %>") //<-
+      }))
+      .pipe($.uglify())
+      .pipe(gulp.dest(setting.path.js.dest));
+  }
+});
+
+// CSS Minify
+gulp.task('cssminify', function(){
+  if(setting.minify.css){
+    return gulp.src(setting.path.sass.dest+'**/*.css')
+      .pipe($.plumber({
+        errorHandler: $.notify.onError("Error: <%= error.message %>") //<-
+      }))
+      .pipe($.csso())
+      .pipe(gulp.dest(setting.path.sass.dest));
+  }
+});
+
+// CSS Beautify
+gulp.task('cssbeautify', function(){
+  if(!setting.cssbeautify.disabled && !setting.minify.css){
+    return gulp.src(setting.path.sass.dest+'**/*.css')
+      .pipe($.plumber({
+        errorHandler: $.notify.onError("Error: <%= error.message %>") //<-
+      }))
+      .pipe($.cssbeautify(setting.cssbeautify.options))
+      .pipe(gulp.dest(setting.path.sass.dest));
+  }
+});
+
+// CSS Comb
+gulp.task('csscomb', function(){
+  if(!setting.csscomb.disabled && !setting.minify.css){
+    return gulp.src(setting.path.sass.dest+'**/*.css')
+      .pipe($.plumber({
+        errorHandler: $.notify.onError("Error: <%= error.message %>") //<-
+      }))
+      .pipe($.csscomb())
+      .pipe(gulp.dest(setting.path.sass.dest));
+  }
+});
+
+// Clean
+gulp.task('clean', del.bind(null, setting.path.base.dest));
+
+// Build
+gulp.task('build', function(){
+  return runSequence(
+    ['clean'],
+    ['html', 'js', 'scss', 'lib', 'include', 'etc'],
+    ['csscomb'],
+    ['imagemin', 'cssminify', 'jsminify', 'cssbeautify']
+    );
+});
+
+// Watch
+gulp.task('watch', function(){
+  browserSync.init(setting.browserSync);
+
+  gulp.watch([setting.path.sass.src], ['scss']);
+  gulp.watch([setting.path.js.src], ['js']);
+  gulp.watch([setting.path.lib.src], ['lib']);
+  gulp.watch([setting.path.include.src], ['include']);
+  gulp.watch([setting.path.etc.src], ['etc']);
+  gulp.watch([setting.path.html.src], ['html']);
+  gulp.watch([setting.path.image.src], ['imagemin']);
 });
 
 gulp.task('default',['watch']);
