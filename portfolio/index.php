669a037904901aae9a1f8f0f77caec14c1527a65
kk-watanabe <kw4869ify@gmail.com>
2017-06-19 19:14:59 +0900

    ポートフォリオを追加

---
+++ portfolio/index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta name="robots" content="none">
+<meta charset="UTF-8">
+<meta name="description" content="渡部和樹が前社で対応してきた案件をまとめたのポートフォリオサイトになります。">
+<meta name="keywords" content="">
+<title>渡部和樹のポートフォリオ</title>
+<link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700" rel="stylesheet">
+<link href="https://fonts.googleapis.com/earlyaccess/notosansjapanese.css" rel="stylesheet" />
+<link href="/assets/css/portfolio.css" rel="stylesheet">
+</head>
+<body>
+
+<div id="container">
+	<header id="header">
+		<h1 class="site-name ff-droid">Portfolio</h1>
+	</header>
+
+	<main id="main">
+		<article class="site">
+			<a href="http://good-creator.jp/" target="_blank" class="anchor">
+				<div class="thum"><img src="img/img01.jpg" width="80" height="60" alt=""></div>
+				<div class="detail">
+					<div class="around">2017年３月頃</div>
+					<h2 class="ttl">GOOD!CREATOR</h2>
+					<p class="work">メインコーディング、CSS設計、JS実装、WPの実装</p>
+					<p class="summary">RWDサイト。一人で全て対応したサイト。仕様が定まってなく変更が多かったが対応できるように制作しました。</p>
+				</div>
+			</a>
+		</article>
+
+		<article class="site">
+			<a href="http://www.j-kamata.com/" target="_blank" class="anchor">
+				<div class="thum"><img src="img/img05.jpg" width="80" height="60" alt=""></div>
+				<div class="detail">
+					<div class="around">2016年12月頃</div>
+					<h2 class="ttl">ジュエリーかまた</h2>
+					<p class="work">メインコーディング、CSS設計、JS実装、WPの実装</p>
+					<p class="summary">RWDサイト。既存のサイトのコーディングを活かしながら各種コーディングしたサイト。流用箇所が多く影響しないように設計するのが大変でした。</p>
+				</div>
+			</a>
+		</article>
+
+		<article class="site">
+			<a href="https://cadjob.co.jp/" target="_blank" class="anchor">
+				<div class="thum"><img src="img/img04.jpg" width="80" height="60" alt=""></div>
+				<div class="detail">
+					<div class="around">2016年10月頃</div>
+					<h2 class="ttl">CADJOB</h2>
+					<p class="work">メインコーディング、CSS設計、JS実装</p>
+					<p class="summary">RWDサイト。システムは社内システム担当者が実装を行ったので各部分相談しながら調整することができました。</p>
+				</div>
+			</a>
+		</article>
+
+		<article class="site">
+			<a href="https://www.kixdutyfree.jp/" target="_blank" class="anchor">
+				<div class="thum"><img src="img/img03.jpg" width="80" height="60" alt=""></div>
+				<div class="detail">
+					<div class="around">2017年３月頃</div>
+					<h2 class="ttl">関西国際空港 直営免税店 出発前予約サイト</h2>
+					<p class="work">メインコーディング、CSS設計、JS実装</p>
+					<p class="summary">PC/SP別々。システムの実装は他社が行うということで事前確認を行いながら制作を進めました。</p>
+				</div>
+			</a>
+		</article>
+
+		<article class="site">
+			<a href="https://919.jp/" target="_blank" class="anchor">
+				<div class="thum"><img src="img/img06.jpg" width="80" height="60" alt=""></div>
+				<div class="detail">
+					<div class="around">2016年6月頃</div>
+					<h2 class="ttl">株式会社クイック</h2>
+					<p class="work">メインコーディング、CSS設計、JS実装、WPの実装</p>
+					<p class="summary">RWDサイト。複数人で作業を行いました。会社のガイドラインを守りつつ複数人で制作しても問題ないように設計しました。</p>
+				</div>
+			</a>
+		</article>
+
+		<article class="site">
+			<a href="http://respect-pal.jp/" target="_blank" class="anchor">
+				<div class="thum"><img src="img/img02.jpg" width="80" height="60" alt=""></div>
+				<div class="detail">
+					<div class="around">2016年３月頃</div>
+					<h2 class="ttl">株式会社リスペクト</h2>
+					<p class="work">メインコーディング、CSS設計、JS実装</p>
+					<p class="summary">RWDサイト。一人で全て対応したサイト。フルサイズのRWDの対応はあまり経験ないので苦労したがいい経験となりました。</p>
+				</div>
+			</a>
+		</article>
+	</main><!-- /#main -->
+
+	<footer id="footer">
+		<small class="copyright">© 2017 K.W.portfolio inc.</small>
+	</footer>
+</div>
+
+<script src="/assets/js/common.js"></script>
+</body>
+</html>
