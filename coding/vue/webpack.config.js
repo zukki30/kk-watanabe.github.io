783fd3583857cf09660ad90f5a13cdc313a70d11
k-watanabe <k-watanabe@early-cross.co.jp>
2017-12-14 11:07:33 +0900

    vue.jsの準備

---
+++ coding/vue/webpack.config.js
@@ -0,0 +1,37 @@
+var config = require('./config');
+var _base = config.base;
+var _setting = config.setting;
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  cache: true,
+  entry: './' + _setting.path.js.src + 'main.js',
+  output: {
+    path: path.join(__dirname, _setting.path.js.dest),
+    filename: 'bundle.js'
+  },
+  devtool: '#source-map',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({sourceMap: true})
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use:[
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['env', {'modules': false}],
+                'react'
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
