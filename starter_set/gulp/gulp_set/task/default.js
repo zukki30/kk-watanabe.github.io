aeb2f740dfb7da13258a7c38e53543f2d8820929
k-watanabe <k-watanabe@early-cross.co.jp>
2018-09-20 10:36:02 +0900

    watch & build を分割

---
+++ starter_set/gulp/gulp_set/task/default.js
@@ -0,0 +1,29 @@
+const gulp    = require('gulp');
+const config  = require('../config');
+const setting = config.setting;
+const $       = require('gulp-load-plugins')(config.loadPlugins);
+
+// Clean
+gulp.task('clean', $.del.bind(null, setting.path.base.dest));
+
+// Build
+gulp.task('build', () => {
+  return $.sequence(
+    ['clean'],
+    ['html', 'js', 'scss', 'include'],
+    ['imagemin', 'jsminify']
+    );
+});
+
+// Watch
+gulp.task('watch', () => {
+  $.browserSync.init(setting.browserSync);
+
+  gulp.watch([setting.path.sass.src], ['scss']);
+  gulp.watch([setting.path.js.src], ['js']);
+  gulp.watch([setting.path.include.src], ['include']);
+  gulp.watch([setting.path.html.src], ['html']);
+  gulp.watch([setting.path.image.src], ['imagemin']);
+});
+
+gulp.task('default',['watch']);
--- starter_set/gulp/gulpfile.js
+++ starter_set/gulp/gulpfile.js
@@ -3,33 +3,3 @@ const requireDir = require('require-dir');
 
 // タスクの読み込み
 requireDir('./gulp_set/task/', {recurse: true});
-
-const gulp    = require('gulp');
-const config  = require('./gulp_set/config');
-const setting = config.setting;
-const $       = require('gulp-load-plugins')(config.loadPlugins);
-
-// Clean
-gulp.task('clean', $.del.bind(null, setting.path.base.dest));
-
-// Build
-gulp.task('build', () => {
-  return $.sequence(
-    ['clean'],
-    ['html', 'js', 'scss', 'include'],
-    ['imagemin', 'jsminify']
-    );
-});
-
-// Watch
-gulp.task('watch', () => {
-  $.browserSync.init(setting.browserSync);
-
-  gulp.watch([setting.path.sass.src], ['scss']);
-  gulp.watch([setting.path.js.src], ['js']);
-  gulp.watch([setting.path.include.src], ['include']);
-  gulp.watch([setting.path.html.src], ['html']);
-  gulp.watch([setting.path.image.src], ['imagemin']);
-});
-
-gulp.task('default',['watch']);
