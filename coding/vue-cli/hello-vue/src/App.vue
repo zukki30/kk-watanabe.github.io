a9c0491e7e302505eed3ac5606b5836d4a81f348
kk-watanabe <kw4869ify@gmail.com>
2018-12-13 14:53:53 +0900

    vue stady form

--- coding/vue-cli/hello-vue/src/App.vue
+++ coding/vue-cli/hello-vue/src/App.vue
@@ -1,48 +1,81 @@
 <template>
     <div class="container">
-        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
-        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
-        <app-quote-grid :quotes="quotes" @quoteDelited="deleteQuote"></app-quote-grid>
+        <form v-if="!submitted">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <!-- Exercise 1 -->
+                    <!-- Create a Signup Form where you retrieve the following Information -->
+                    <!-- Full Name (First Name + Last Name) -->
+                    <!-- Mail -->
+                    <!-- Password -->
+                    <!-- Store Data? Yes/No -->
 
-        <div class="row">
-            <div class="col-sm-12 text-center">
-                <div class="alert alert-info">Info: Click on a Quote to Delte it!</div>
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
+                    <app-full-name v-model="fullName"></app-full-name>
+                    <div class="form-group">
+                        <label>Mail</label>
+                        <input type="text" class="form-control" v-model="email">
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" v-model="password">
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            <input type="radio" value="Yes" v-model="storeData"> Yes
+                        </label>
+                        <label>
+                            <input type="radio" value="No" v-model="storeData"> No
+                        </label>
+                    </div>
+                    <!-- Exercise 2 -->
+                    <!-- Only display the Form if it has NOT been submitted -->
+                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+                    <button
+                            type="submit"
+                            @click.prevent="submitted = true"
+                            class="btn btn-primary">Submit the Form</button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="submitted">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{ fullName }}</p>
+                        <p>Mail: {{ email }}</p>
+                        <p>Password: {{ password }}</p>
+                        <p>Store in Database?: {{ storeData }}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import QuoteGrid from './components/QuoteGrid'
-    import NewQuote from './components/NewQuote'
-    import Header from './components/Header'
+    import FullName from './FullName.vue';
 
     export default {
-        data: function() {
+        data() {
             return {
-                quotes: [
-                    'Just a Quote to see something'
-                ],
-                maxQuotes: 10
+                fullName: 'Max SchwarzmÃ¼ller',
+                email: '',
+                password: '',
+                storeData: 'Yes',
+                submitted: false
             }
         },
-        methods: {
-            newQuote(quote) {
-                if (this.quotes.length >= this.maxQuotes) {
-                    return alert('Please delete Quotes first!');
-                }
-                this.quotes.push(quote);
-            },
-             deleteQuote(index) {
-                 this.quotes.splice(index, 1)
-             }
-        },
         components: {
-            appQuoteGrid: QuoteGrid,
-            appNewQuote: NewQuote,
-            appHeader: Header
+            appFullName: FullName
         }
-    } 
+    }
 </script>
 
 <style>
